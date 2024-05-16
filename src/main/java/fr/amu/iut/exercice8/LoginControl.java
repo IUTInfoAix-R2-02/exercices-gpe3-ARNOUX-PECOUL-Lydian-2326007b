b1b662318dd41545076204cef430f3b64a3670f5
Lydian <lydian.ARNOUX-PECOUL@etu.univ-amu.fr>
2024-05-16 17:19:51 +0200

    exercice 8 fini

--- src/main/java/fr/amu/iut/exercice8/LoginControl.java
+++ src/main/java/fr/amu/iut/exercice8/LoginControl.java
@@ -1,15 +1,27 @@
 package fr.amu.iut.exercice8;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 
 public class LoginControl extends GridPane {
 
     @FXML
+    TextField usr;
+
+    @FXML
+    PasswordField pwd;
+
+    @FXML
     private void okClicked() {
+        System.out.println(usr.getText());
     }
 
     @FXML
     private void cancelClicked() {
+        usr.setText("");
+        pwd.setText("");
     }
 }
