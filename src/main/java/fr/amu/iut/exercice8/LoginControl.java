576399486577ffb3ea55a21e77edd3e1bf656543
Lydian <lydian.ARNOUX-PECOUL@etu.univ-amu.fr>
2024-05-16 17:04:35 +0200

    exercice 8 en cours

--- src/main/java/fr/amu/iut/exercice8/LoginControl.java
+++ src/main/java/fr/amu/iut/exercice8/LoginControl.java
@@ -1,12 +1,15 @@
 package fr.amu.iut.exercice8;
 
+import javafx.fxml.FXML;
 import javafx.scene.layout.GridPane;
 
 public class LoginControl extends GridPane {
 
+    @FXML
     private void okClicked() {
     }
 
+    @FXML
     private void cancelClicked() {
     }
 }
