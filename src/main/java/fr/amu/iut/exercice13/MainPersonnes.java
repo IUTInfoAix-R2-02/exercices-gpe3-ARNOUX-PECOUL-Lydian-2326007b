3dd016c13a8514d788440c1e018cac8bd5621c64
Lydian <lydian.ARNOUX-PECOUL@etu.univ-amu.fr>
2024-05-27 11:54:07 +0200

    ex 13

--- src/main/java/fr/amu/iut/exercice13/MainPersonnes.java
+++ src/main/java/fr/amu/iut/exercice13/MainPersonnes.java
@@ -1,23 +1,50 @@
-package fr.amu.iut.exercice3;
+package fr.amu.iut.exercice13;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
+
+import java.util.List;
 
 @SuppressWarnings("Duplicates")
 public class MainPersonnes  {
 
     private static ObservableList<Personne> lesPersonnes;
 
+    private static List<? extends Personne> temp;
+
     private static ListChangeListener<Personne> unChangementListener;
+
+    private static ListChangeListener<Personne> plusieursChangementsListener;
 
     public static void main(String[] args) {
 
         lesPersonnes = FXCollections.observableArrayList();
 
-//        unChangementListener = à completer
+        plusieursChangementsListener = new ListChangeListener<Personne>() {
+            @Override
+            public void onChanged(Change<? extends Personne> change) {
+                change.next();
+                boolean vA = change.wasAdded();
+                boolean vR = change.wasRemoved();
+                if (vA==true){
+                    temp = change.getAddedSubList();
+                    for (int i = 0; i<change.getAddedSubList().size(); i+=1){
+                        System.out.println("Ajouté : " + temp.get(i).getNom());
+                    }
 
-        lesPersonnes.addListener(unChangementListener);
+                }
+                if (vR==true){
+                    temp = change.getRemoved();
+                    for (int i = 0; i<temp.size(); i+=1){
+                        System.out.println("Supprimé : " + temp.get(i).getNom());
+                    }
+                }
+            }
+        };
+
+        lesPersonnes.addListener(plusieursChangementsListener);
+        question5();
     }
 
     public static void question1() {
